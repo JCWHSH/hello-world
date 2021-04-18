febf0c5c1bc994e18babc7f13aef61a109814e60
JCWHSH <81768351+JCWHSH@users.noreply.github.com>
2021-04-18 12:10:47 +0200

    Update array.c
    Weitere Kommentare eingefügt

--- array.c
+++ array.c
@@ -6,7 +6,7 @@ int main() {
 	int aiFeld[5] = {3,2,1}; //Array mit 5 Werten
 	
 	int i; 
-	for (i=0; i<5;i++) {
+	for (i=0; i<5;i++) {  //schleife, die fünf mal durchlaufen wird
 		aiFeld[i]=aiFeld[i+1];
 		printf("%d ", aiFeld[i]);
 	}
